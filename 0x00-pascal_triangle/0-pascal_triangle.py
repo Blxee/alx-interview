3d3d0c5b37d11fdb3a0358695f33f24fe837c3e4
Blxee <aminedev57@gmail.com>
2024-01-02 12:50:02 +0100

    Added: pascal triangle solution

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""0. Pascal's Triangle"""
+
+def pascal_triangle(n):
+    """Creates and returns a pascal triangle of height n as 2D list.
+
+    Args:
+        n (int): the height of the triangle.
+
+    Returns:
+        list[list]: A 2d list representing triangle of height n.
+
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [0, *triangle[i - 1], 0]
+        triangle.append( [
+            a + b
+            for a, b in zip(row[:-1], row[1:])
+        ])
+    return triangle
---
+++ README.md
@@ -0,0 +1,3 @@
+# ALX Interview:
+
+Repository for all interview related questions (as separate projects)
