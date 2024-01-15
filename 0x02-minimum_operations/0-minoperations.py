5af402b063b44a9cee481bdf2006271c7b8b8629
Blxee <aminedev57@gmail.com>
2024-01-15 11:53:45 +0100

    Added: only task of 0x02. Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""0. Minimum Operations"""
+
+
+def minOperations(n):
+    """Determines the minimum number of operations to write H n times.
+
+    Args:
+        n (int): number of H characters to achieve.
+    Returns:
+        int: minimum number of operations, or 0.
+    """
+    if n == 1:
+        return 0
+    op = 2
+    while n % op != 0:
+        op += 1
+    return op + minOperations(int(n / op))
