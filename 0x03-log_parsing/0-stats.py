7b16e76a69840b760e6f2d0a1080d795dda667ce
Blxee <aminedev57@gmail.com>
2024-01-24 02:50:16 +0100

    Added: only task of 0x03. Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""0. Log parsing"""
+import re
+import signal
+import sys
+
+
+if __name__ == "__main__":
+    pattern = re.compile(r'(?P<ip>.+) - \[(?P<date>.+)\] "GET /projects/260 HTTP/1.1" (?P<code>.+) (?P<size>\d+)')
+
+    status_codes = {
+        code: 0
+        for code in (200, 301, 400, 401, 403, 404, 405, 500)
+    }
+    total_size = 0
+
+    def handler(sig=None, frame=None):
+        """Halndler for the interrupt (<C>-C) key."""
+        print('File size:', total_size)
+        for code, n in status_codes.items():
+            if n:
+                print(f'{code}: {n}')
+
+    signal.signal(signal.SIGINT, handler)
+
+    i = 0
+    for line in sys.stdin:
+        i += 1
+        match = re.match(pattern, line)
+        if match:
+            code = int(match.group('code'))
+            size = int(match.group('size'))
+            if code in status_codes:
+                status_codes[code] += 1
+            total_size += size
+            if i % 10 == 0:
+                handler()
