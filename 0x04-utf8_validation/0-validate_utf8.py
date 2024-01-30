2c26319992fb7f95df16a1cf2a096a6a58f7152d
Blxee <aminedev57@gmail.com>
2024-01-30 07:55:49 +0100

    Added: only task of 0x04. UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""0. UTF-8 Validation"""
+
+
+def validUTF8(data):
+    """Determines if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data (list[int]): the data to be ckecked.
+
+    Returns:
+        True if data is a valid UTF-8 encoding, else return False.
+    """
+    if len(data) == 0:
+        return False
+    for byte in data:
+        if not (0 <= byte <= 255):
+            return False
+    return True
