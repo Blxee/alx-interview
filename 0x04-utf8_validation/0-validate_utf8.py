70a58a2d64a334a1f233212ad18259fa10a5cc13
Blxee <aminedev57@gmail.com>
2024-01-30 08:33:11 +0100

    Fixed: only task of 0x04. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,15 +11,16 @@ def validUTF8(data):
     Returns:
         True if data is a valid UTF-8 encoding, else return False.
     """
+    skip
     for i, byte in enumerate(data):
         if not (0 <= byte <= 255):
             return False
         if i == 0:
             if not (
-                byte & 0b1 != 0b0
-                or byte & 0b111 != 0b110
-                or byte & 0b1111 != 0b1110
-                or byte & 0b11111 != 0b11110
+                byte & 0b1 == 0b0
+                or byte & 0b111 == 0b110
+                or byte & 0b1111 == 0b1110
+                or byte & 0b11111 == 0b11110
             ):
                 return False
     return True
