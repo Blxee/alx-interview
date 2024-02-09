a3d28ef18dd90b2127bebe422545a0d766934c46
Blxee <aminedev57@gmail.com>
2024-02-09 02:50:16 +0100

    Added: progress of the task

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,79 +3,34 @@
 from sys import argv
 
 
-ADD, REMOVE = 1, -1
-def move_queen(queen: list[int],
-               board: list[list],
-               action: int) -> bool:
-    """Adds or removes a queen from the board.
+def can_move(pos1: list[int], pos2: list[int]) -> bool:
+    """Determines whether a queen is a target of another."""
+    x1, y1 = pos1
+    x2, y2 = pos2
+    return x1 == x2 or y1 == y2 or abs(x1 - x2) == abs(y1 - y2)
 
-    Args:
-        queen: a list of len 2 representing queen x and y position.
-        board: 2D list representing the board.
-        action: action to take, can be either ADD or REMOVE.
 
-    Returns:
-        True if action was successful, else False.
-    """
-    x, y = queen
-    # add or remove queen if allowed.
-    if action == ADD:
-        if type(board[x][y]) == list or board[x][y] > 0:
-            return False
-        else:
-            board[x][y] = queen
-    elif action == REMOVE: 
-        if type(board[x][y]) != list:
-            return False
-        else:
-            board[x][y] = 0
-    stack = []
-    ret = True
-    # iterate through the board and threaten empty cells.
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if (i, j) == (x, y):
+stack = []
+
+
+def n_queens(n: int, queens: list[list[int]] = []) -> None:
+    """Prints all possiple solutions of n-queens problem."""
+    for x in range(n):
+        for y in range(n):
+            if [x, y] in queens:
                 continue
-            # if cell is within queen's move-set.
-            if i == x or j == y or abs(x - i) == abs(y - j):
-                # if cell is not empty, break.
-                if action == ADD and type(board[i][j]) == list:
-                    ret = False
-                    board[x][y] = 0
+            for qx, qy in queens:
+                if can_move([qx, qy], [x, y]):
                     break
-                board[i][j] += action
-                stack.append((i, j))
-        if not ret: break
-    # if loop was broken.
-    if not ret:
-        # unthreaten all threatened cells.
-        for i, j in stack:
-            board[i][j] -= action
-    # return whether action has succeded
-    return ret
-
-
-def n_queens(n: int) -> list[list[list[int]]]:
-    # make a 2D array of n * n init to 0.
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    solution_list = []
-    # add new new solutions until there are none.
-    while 1:
-        solution = []
-        while len(solution) < n:
-            cell_found = False
-            for x in range(n):
-                for y in range(n):
-                    if move_queen([x, y], board, ADD):
-                        solution.append([x, y])
-                        cell_found = True
-                        break
-                if cell_found: break
-            if not cell_found:
-                move_queen(solution.pop(), board, REMOVE)
-        solution_list.append(solution)
-        break # DEBUG
-    return solution_list
+            else:
+                new_queens = queens + [[x, y]]
+                if len(new_queens) == n:
+                    s = set([tuple(q) for q in new_queens])
+                    if s not in stack:
+                        print(new_queens)
+                        stack.append(s)
+                else:
+                    n_queens(n, new_queens)
 
 
 if __name__ == '__main__':
@@ -95,4 +50,4 @@ if __name__ == '__main__':
         print('N must be at least 4')
         exit(1)
 
-    print(*n_queens(n), sep='\n')
+    n_queens(n)
