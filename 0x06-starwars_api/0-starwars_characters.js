5fd9cf56dab465c2694245b820f239470621ab16
Blxee <aminedev57@gmail.com>
2024-02-14 07:38:47 +0100

    Fixed: only task of 0x06. Starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,27 +3,23 @@ const request = require('request');
 
 const film = Number.parseInt(process.argv[2]);
 
-async function main () {
-  request(`https://swapi-api.alx-tools.com/api/films/${film}/`, async (err, res, body) => {
-    if (err) {
-      process.exit(1);
-    }
-    const filmInfo = JSON.parse(body);
-    const charactersArray = filmInfo.characters;
+request(`https://swapi-api.alx-tools.com/api/films/${film}/`, async (err, res, body) => {
+  if (err) {
+    process.exit(1);
+  }
+  const filmInfo = JSON.parse(body);
+  const charactersArray = filmInfo.characters;
 
-    for (const characterURL of charactersArray) {
-      await new Promise((resolve, reject) => {
-        request(characterURL, (err2, res2, body2) => {
-          if (err2) {
-            reject(err2);
-          }
-          const character = JSON.parse(body2);
-          console.log(character.name);
-          resolve(character.name);
-        });
+  for (const characterURL of charactersArray) {
+    const name = await new Promise((resolve, reject) => {
+      request(characterURL, (err2, res2, body2) => {
+        if (err2) {
+          reject(err2);
+        }
+        const character = JSON.parse(body2);
+        resolve(character.name);
       });
-    }
-  });
-}
-
-main();
+    });
+    console.log(name);
+  }
+});
