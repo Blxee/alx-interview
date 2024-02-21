e5dac63a987e366215d11476c2a451584f61c323
Blxee <aminedev57@gmail.com>
2024-02-21 02:11:38 +0100

    Added: initial solution file template

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Main module for solution to 2d matrix rotation."""
+
+
+def rotate_2d_matrix(matrix):
+    """Roatates 2d matrix in-place."""
+    original = matrix.copy()
+    width, height = len(matrix[0]), len(matrix)
+    for x in range(width):
+        for y in range(height):
+            matrix[x][y] = original[height - 1 - y][x]
