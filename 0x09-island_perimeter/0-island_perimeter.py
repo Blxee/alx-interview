707501ffdfd00b830191cbab856c9ed69c71fa47
Blxee <aminedev57@gmail.com>
2024-03-05 11:43:54 +0100

    Added: only task of island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Island perimeter solution module."""
+
+
+def island_perimeter(grid):
+    perimeter = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            cell = grid[x][y]
+            if cell == 1:
+                for i, j in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
+                    if not (0 <= i < len(grid)) or not (0 <= j < len(grid[0])):
+                        continue
+                    cell = grid[i][j]
+                    if cell == 0:
+                        perimeter += 1
+    return perimeter
